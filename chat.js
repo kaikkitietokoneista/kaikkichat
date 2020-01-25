8855abb2a35e6ce64ebbf213d4a4db1360144a98
Kaikkitietokoneista.net <info@kaikkitietokoneista.net>
2020-01-25 17:12:50 +0200

    Estetty spämmintä

--- chat.js
+++ chat.js
@@ -2,59 +2,84 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var xss = require('xss');
-const SocketAntiSpam  = require('socket-anti-spam')
-
-//Antispamin konfigurointi
-const socketAntiSpam = new SocketAntiSpam({
-   banTime:            30,         // Ban time in minutes
-   kickThreshold:      2,          // User gets kicked after this many spam score
-   kickTimesBeforeBan: 3,          // User gets banned after this many kicks
-   banning:            true,       // Uses temp IP banning after kickTimesBeforeBan
-   io:                 socket-io,  // Bind the socket.io variable
- })
-
-
-/*SIIRRÄ HASHIEN TARKASTUS JA KAKKI MUUKIN FUNCTIONEIKSI */
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+var rajoitustaulukko = new Object();
+var rajoitustaulukkoip = new Object();
+//Ei tarvita var kylmennäip = new Object();
+
+function pyyhirajoitettavaipyksi(ip) {
+  rajoitustaulukko[ip] -= 1;
+}
+
+function luouusirajoitettavaip(ip) {
+  if (rajoitustaulukko[ip] == null) {
+    rajoitustaulukko[ip] = 0;
+  } else {
+    rajoitustaulukko[ip] += 1;
+  }
+  setTimeout(function() {pyyhirajoitettavaipyksi(ip)}, 60000);
+}
+
+
+function pyyhirajoitettava(id) {
+  rajoitustaulukko[id] = -1;
+}
+
+function luouusirajoitettava(id) {
+  rajoitustaulukko[id] = 0;
+  setTimeout(function() {pyyhirajoitettava(id)}, 2000);
+}
+
 var kayttajienmaara = 0;
 
 io.on('connection', function(socket){
+  var ip = socket.handshake.address;
+  if (rajoitustaulukko[ip] == 3) {
+    socket.emit("Olet rajoitettu.");
+    socket.disconnect()
+  } else {
+    luouusirajoitettava(socket.id)
+    console.log('Käyttäjä liittyi.');
+    kayttajienmaara = kayttajienmaara + 1;
+    io.emit('palvelinviesti', '<center>Uusi käyttäjä liittyi joukkoomme. <br> Paikalla on tällä hetkellä: ' + kayttajienmaara + ' käyttäjää</center>');
 
-  console.log('Käyttäjä liittyi.');
-  kayttajienmaara = kayttajienmaara + 1;
-  io.emit('palvelinviesti', '<center>Uusi käyttäjä liittyi joukkoomme. <br> Paikalla on tällä hetkellä: ' + kayttajienmaara + ' käyttäjää</center>');
+    socket.on('huoneviesti', (viesti, huone) => {
+      console.log(rajoitustaulukko);
+      var id = socket.id;
+      if (rajoitustaulukko[id] == -1) {
+        if (viesti != "") {
+          io.emit('huoneviesti', viesti)
+          //EI TOIMI TÄLLÄ HETKELLÄ: io.to(huone).emit(viesti);
+          luouusirajoitettava(socket.id)
+        }
+      } else {
+        socket.emit('Hillitse viestiesi määrää!')
+      }
+    });
 
-  socket.on('huoneviesti', (viesti, huone) => {
-    if (viesti != "") { //Viesti sisältää nimen joten poista se
-      io.emit('huoneviesti', viesti)
-      //EI TOIMI TÄLLÄ HETKELLÄ: io.to(huone).emit(viesti);
-    }
-    //console.log(viesti);
-    //console.log(huone);
-  });
+    socket.on('disconnect', function() {
+      console.log('Käyttäjä poistui.');
+      luouusirajoitettavaip(socket.handshake.address)
+      kayttajienmaara = kayttajienmaara - 1;
+      io.emit('palvelinviesti', '<center>Käyttäjä poistui palvelimelta. <br> Paikalla on tällä hetkellä: ' + kayttajienmaara + ' käyttäjää</center>');
+    })
 
-  socket.on('disconnect', function() {
-    console.log('Käyttäjä poistui.');
+    socket.join('yleinen', () => {
+      io.to('yleinen').emit('<center>Uusi käyttäjä liittyi!</center>');
+    })
 
-    kayttajienmaara = kayttajienmaara - 1;
-    io.emit('palvelinviesti', '<center>Käyttäjä poistui palvelimelta. <br> Paikalla on tällä hetkellä: ' + kayttajienmaara + ' käyttäjää</center>');
-  })
+    socket.on('kirjoittaa', function(kuka) {
+      io.emit('kirjoittaa', xss(kuka))
+    })
 
-  socket.join('yleinen', () => {
-    io.to('yleinen').emit('<center>Uusi käyttäjä liittyi!</center>');
-  })
-
-  socket.on('kirjoittaa', function(kuka) {
-    io.emit('kirjoittaa', xss(kuka))
-  })
-
-  socket.on('lopetti', function(kuka) {
-    io.emit('lopetti', xss(kuka))
-  })
+    socket.on('lopetti', function(kuka) {
+      io.emit('lopetti', xss(kuka))
+    })
+  }
 });
 
 http.listen(3000, function(){
